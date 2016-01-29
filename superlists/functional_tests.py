4749ce764e3b104707199d275b1fc62e63c226f9
Ella Holmes <holme22e@mtholyoke.edu>
2016-01-29 14:51:01 -0500

    Unit test passing for root url

--- superlists/functional_tests.py
+++ superlists/functional_tests.py
@@ -5,13 +5,15 @@ class NewVisitorTest(unittest.TestCase):
 
     def setUp(self):
         self.browser = webdriver.Firefox()
+        self.browser.implicitly_wait(3)
 
     def tearDown(self):
         self.browser.quit()
 
     def test_user_can_enter_in_travel_plans(self):
         self.browser.get('http://localhost:8000')
-        self.assertIn('Django', self.browser.title)
+        self.assertIn('Carpool', self.browser.title)
+        self.fail('Finish thet test!')
 
 if __name__ == '__main__':
     unittest.main(warnings='ignore')
