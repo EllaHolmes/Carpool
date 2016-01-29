c202938854410e257496f4fd88946b9dc9254cde
Ella Holmes <holme22e@mtholyoke.edu>
2016-01-29 14:17:12 -0500

    create first functional test

--- superlists/functional_tests.py
+++ superlists/functional_tests.py
@@ -1,5 +1,17 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
-assert 'Django' in browser.title
+class NewVisitorTest(unittest.TestCase):
+
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_user_can_enter_in_travel_plans(self):
+        self.browser.get('http://localhost:8000')
+        self.assertIn('Django', self.browser.title)
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
