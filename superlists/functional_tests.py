8b6f8375246229b347ccb5891da87bbd35201b81
imann24 <imann24@gmail.com>
2016-02-04 22:36:35 -0500

    Developing searching for ride test

--- superlists/functional_tests.py
+++ superlists/functional_tests.py
@@ -2,6 +2,7 @@ from selenium import webdriver
 import unittest
 
 class NewVisitorTest(unittest.TestCase):
+    
     find_ride_page_title = "Find a Ride"
 
     def setUp(self):
@@ -20,18 +21,25 @@ class NewVisitorTest(unittest.TestCase):
 
     def navigate_to_find_a_ride_page (self):
         #TODO loads find a ride page
+        pass
 
-    #TODO check that user is logged in
+    def user_is_logged_in (self):
+        #TODO check that user is logged in
+        return True;
     
     def test_user_can_search_for_ride (self):
         #User opens up our website
         self.browser.get('http://localhost:8000')
 
+        #Check that the user is logged in
+        if (not self.user_is_logged_in()):
+            self.fail("User is not logged in")
+
         #User navigates to find a ride page
         self.navigate_to_find_a_ride_page()
 
-        #User sees title page "Find a Ride"
-        self.assertIn(find_ride_page_title, self.browser.title)
+        #User sees page "Find a Ride"
+        self.assertIn(self.find_ride_page_title, self.browser.title)
         header_text = self.browser.find_element_by_tag_name('h1').text
         self.assertIn(find_ride_page_title, header_text)
 
@@ -42,18 +50,21 @@ class NewVisitorTest(unittest.TestCase):
             'Enter First and Last name'
         )
 
+        #User enters her start location
         inputStart = self.browser.find_element_by_id('id_start')
         self.assertEqual(
             inputStart.get_attribute('placeholder'),
             'Start Location'
         )
 
+        #User enters her end location
         inputEnd = self.browser.find_element_by_id('id_end')
         self.assertEqual(
             inputEnd.get_attribute('placeholder'),
             'End Location'
         )
 
+        #User enters her travel date
         inputDate = self.browser.find_element_by_id('id_date')
         self.assertEqual(
             inputDate.get_attribute('placeholder'),
