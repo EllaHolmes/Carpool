3b6a480ee1522386604435bab7b215e421d70ffa
Ella Holmes <holme22e@mtholyoke.edu>
2016-02-05 14:30:54 -0500

    deleted date specification and added a button

--- superlists/functional_tests.py
+++ superlists/functional_tests.py
@@ -14,17 +14,15 @@ class NewDriverVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
-    def enter_new_carpool_driver(self, firstName, lastName, startLocation, endLocation, date):
+    def enter_new_carpool_driver(self, firstName, lastName, startLocation, endLocation):
         inputFirstName = self.browser.find_element_by_id('id_first_name')
         inputLastName = self.browser.find_element_by_id('id_last_name')
         inputStart = self.browser.find_element_by_id('id_start')
         inputEnd = self.browser.find_element_by_id('id_end')
-        inputDate = self.browser.find_element_by_id('id_date')
         inputFirstName.send_keys(firstName);
         inputLastName.send_keys(lastName);
         inputStart.send_keys(startLocation);
         inputEnd.send_keys(endLocation);
-        inputDate.send_keys(date);
         # enterButton = self.browser.find_element_by_id('id_new_driver_btn')
         # enterButton.send_key(Keys.ENTER)
 
@@ -38,7 +36,7 @@ class NewDriverVisitorTest(unittest.TestCase):
         header_text = self.browser.find_element_by_tag_name('h1').text
         self.assertIn('Carpool', header_text)
 
-        #She is invited to enter in her name, start location, end location, and date
+        #She is invited to enter in her name, start location, end location
         inputFirstName = self.browser.find_element_by_id('id_first_name')
         self.assertEqual(
             inputFirstName.get_attribute('placeholder'),
@@ -63,14 +61,8 @@ class NewDriverVisitorTest(unittest.TestCase):
             'End Location'
         )
 
-        inputDate = self.browser.find_element_by_id('id_date')
-        self.assertEqual(
-            inputDate.get_attribute('placeholder'),
-            'Travel Date'
-        )
-
         #She enters in her information and presses enter
-        self.enter_new_carpool_driver('Mary', 'Lyon', 'South Hadley, MA', 'Amherst, MA', '1,1,2017')
+        self.enter_new_carpool_driver('Mary', 'Lyon', 'South Hadley, MA', 'Amherst, MA')
 
         # #she is then redirected to a map that shows a list of people looking for a ride
         # map_url = self.browser.current_url
