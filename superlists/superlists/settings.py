e6f4af34195d23167a86cbf13406a751668b1b10
imann24 <imann24@gmail.com>
2016-02-12 01:29:27 -0500

    Got static web page navigation working

--- superlists/superlists/settings.py
+++ superlists/superlists/settings.py
@@ -40,8 +40,10 @@ INSTALLED_APPS = (
     'carpool',
 )
 
+
 MIDDLEWARE_CLASSES = (
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.locale.LocaleMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
