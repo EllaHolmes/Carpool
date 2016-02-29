34d78b4708f14a37a4fbe0fb8d030ef71dbb3bad
Isaiah Mann <imann24@gmail.com>
2016-02-29 15:38:55 -0500

    Continuing to work on the bounding box script

--- superlists/carpool/bounding_box.py
+++ superlists/carpool/bounding_box.py
@@ -4,8 +4,19 @@ class BoundingBox():
 
 	def create(self, start_lat, start_lng, end_lat, end_lng):
 		# Gets the two points
+		self.start = LatLng(start_lat, start_lng)
+		self.end = LatLng(end_lat, end_lng)
+
+		self.top_left_corner = None
+		self.top_right_corner = None
+		self.bottom_left_corner = None
+		self.bottom_right_corner = None
+
+		if start.lat > end.lat and start.lng > end.lng:
+			
 
 		# Determines which corners they are
+
 
 		# Sets them as those two 
 
