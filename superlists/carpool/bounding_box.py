5c8163226ad6a400054e6fe92202985638e83aff
imann24 <imann24@gmail.com>
2016-02-29 15:22:56 -0500

    Working on bounding box script

---
+++ superlists/carpool/bounding_box.py
@@ -0,0 +1,19 @@
+
+class BoundingBox():
+
+
+	def create(self, start_lat, start_lng, end_lat, end_lng):
+		# Gets the two points
+
+		# Determines which corners they are
+
+		# Sets them as those two 
+
+	def get_offset()
+
+
+
+
+class LatLng(lat, lng):
+	self.lat = lat
+	self.lng = lng
