18a2908c534635594401a0b7de6848758de9c0be
imann24 <contact@isaiahmann.com>
2016-04-06 00:26:41 -0400

    Using JavaScript Google Maps API to geocode

--- superlists/carpool/views.py
+++ superlists/carpool/views.py
@@ -1,8 +1,10 @@
+# -*- coding: utf-8 -*-
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from carpool.models import Rider, Driver
 from datetime import date
 import sys
+import parsing
 from geopy.geocoders import Nominatim
 
 debugging = True
@@ -13,7 +15,7 @@ def home_page(request):
         try:
             user_= create_new_rider(request)
             user_.save()
-        except: # catch ​*all*​ exceptions
+        except: # Catch all execeptions
             e = sys.exc_info()[0]
             print( "Error: %s" % e )
             error = "Please enter in all feilds"
@@ -59,12 +61,23 @@ def new_user_page(request):
 
 def create_new_driver(request):
     user_ = Driver()
+
+    start_lat_lng_arr = parsing.parse_lat_lng_string (
+        request.POST['start_lat_lng']
+    )
+
+    end_lat_lng_arr = parsing.parse_lat_lng_string (
+        request.POST['end_lat_lng']
+    )
+
     user_.create(
         request.POST['first_name_text'],
         request.POST['last_name_text'],
         request.POST['start_text'],
         request.POST['end_text'],
-        request.POST['date_text']
+        request.POST['date_text'],
+        start_lat_lng_arr,
+        end_lat_lng_arr
     )
 
     #save the object
@@ -77,12 +90,22 @@ def create_new_driver(request):
 def create_new_rider(request):
     user_ = Rider()
     try:
+        start_lat_lng_arr = parsing.parse_lat_lng_string (
+            request.POST['start_lat_lng']
+        )
+
+        end_lat_lng_arr = parsing.parse_lat_lng_string (
+            request.POST['end_lat_lng']
+        )
+
         user_.create(
             request.POST['first_name_text'],
             request.POST['last_name_text'],
             request.POST['start_text'],
             request.POST['end_text'],
-            request.POST['date_text']
+            request.POST['date_text'],
+            start_lat_lng_arr,
+            end_lat_lng_arr
         )
     except: # catch ​*all*​ exceptions
         e = sys.exc_info()[0]
